f97fd81ec748d9ea3b2b816245cabdffdf8ee0e9
KosukeGoto057 <kousuke510@akane.waseda.jp>
2021-06-04 09:45:36 +0000

    [update]bookers2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,10 +10,15 @@ class BooksController < ApplicationController
   end
 
   def index
+
+    @book = Book.new
+    @book_index = Book.all
   end
 
   def show
-    @book = Book.find(params[:id])
+    @book = Book.new
+    @book_show = Book.find(params[:id])
+    @user = @book_show.user
   end
 
   def edit
@@ -23,10 +28,13 @@ class BooksController < ApplicationController
   end
 
   def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
   end
 
   private
-  
+
   def book_params
     params.require(:book).permit(:title, :body)
   end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '43c7975d762d12d2502b12d82a60c32de02a5e363e6dc1cf6c3382093a1f679ccd518182cac75754e125a67340e29043ad2ec117300c674e20d41e0a28d263ed'
