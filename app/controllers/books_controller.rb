6af9dfb1cf924fe0c6fb8208e7ed12cf6c51df42
KosukeGoto057 <kousuke510@akane.waseda.jp>
2021-06-06 18:01:11 +0000

    [finish]bookers2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,14 +5,20 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to book_path(@book)
+    if @book.save
+      flash[:notice] = "You have created book successfully."
+      redirect_to book_path(@book)
+    else
+      @user = current_user
+      @books = Book.all
+      render :index
+    end
   end
 
   def index
-
+    @user = current_user
     @book = Book.new
-    @book_index = Book.all
+    @books = Book.all
   end
 
   def show
@@ -22,9 +28,20 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
+    if @book.user != current_user
+      redirect_to books_path
+    end
   end
 
   def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "You have updated book successfully."
+      redirect_to book_path(@book)
+    else
+      render :edit
+    end
   end
 
   def destroy
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,13 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   attachment :profile_image
-  
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
+  validates :name, {uniqueness: true, length: {in: 2..20}}
+  validates :introduction, length: {maximum:50}
+
   has_many :books, dependent: :destroy
-  
+
 end
