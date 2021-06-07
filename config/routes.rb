20a1a53fe166d5d931cb6a498cd3ec358b816550
KosukeGoto057 <kousuke510@akane.waseda.jp>
2021-06-07 15:43:20 +0000

    [update]bookers2-favorite

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  get 'users/show'
   devise_for :users
   root to: 'homes#top'
   get 'home/about' => 'homes#about'
-  resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy]
+  resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy] do
+    resource :favorites, only: [:create, :destroy]
+    resources :book_comments, only: [:create, :destroy]
+  end
   resources :users, only: [:index, :show, :edit, :update]
 end
